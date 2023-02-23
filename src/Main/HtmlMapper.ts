7dfb6f44d222fa0338956a01b699ca18902101c3
Oliver <olivercreativeworks@gmail.com>
2023-02-22 20:25:55 -0500

    Update htmlmapper to  work with img tags

--- src/Main/HtmlMapper.ts
+++ src/Main/HtmlMapper.ts
@@ -1,6 +1,7 @@
 interface HtmlMapper{
     addParagraphTags(text:string):string
     addLinkTags(url:string, text:string):string
+    addImageTags(sourceUrl: string):string
 }
 
 export class MyHtmlMapper implements HtmlMapper{
@@ -10,4 +11,16 @@ export class MyHtmlMapper implements HtmlMapper{
     addLinkTags(url: string, text: string): string {
         return `<a href="${url}", target="_blank">${text}</a>`
     }
+    addImageTags(sourceUrl:string, otherProps?:string): string{
+        return `<img src="${sourceUrl}" ${otherProps}>`
+    }
+    static createAttributes(attributeString:string, [attributeName, attributeValue]:[string, string]):string{
+        return attributeString.concat(`${attributeName}:"${attributeValue}"`)
+    }
+    static createAttributesFromAttributesObject(attributesObject:Record<string, string>):string{
+        return Object.entries(attributesObject).reduce(MyHtmlMapper.createAttributes, " ").trim()
+    }
 }
+
+//<a href="https://twitter.com/urbanupbound_ny" target="_blank"><img src="https://cdn.exclaimer.com/Handbook%20Images/twitter-icon_32x32.png?_ga=2.195531820.1874837458.1618585022-1466277378.1618585022"></a>
+//https://pixelsconverter.com/pt-to-px
