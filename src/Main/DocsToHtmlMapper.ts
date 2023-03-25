84c09a44849e7b426fd9b2adba7d0dc27fb2b87d
Oliver <olivercreativeworks@gmail.com>
2023-03-25 19:47:15 -0400

    Transition to html mapper instead of doc mapper.

---
+++ src/Main/DocsToHtmlMapper.ts
@@ -0,0 +1,76 @@
+import { DocsDocumentModel, DocsParagraphElementModel, DocsInlineObjectElementModel } from "../Models/DocsDocumentModel";
+import { DocsInlineObjectsModel } from "../Models/DocsInlineObjectModel";
+import { concatStrings } from "../Models/UtilityFunctions";
+import { List } from "../Monads/List";
+import { Maybe, curryLiftA2 } from "../Monads/Maybe";
+import { bindClassMethodsToClassInstance } from "./Decorator";
+import { HtmlCreatorMapper } from "./HtmlCreator";
+import { ImageHtmlMapper, createImageHtmlFn, Image } from "./ParagraphElements/Image";
+import { DocsDocsDocumentAsMultipleParagraphs, Paragraph, ParagraphElementHtmlMapper, ParagraphHtmlWrapper, wrapInParagraphTagFn } from "./ParagraphElements/ParagraphElements";
+import { TextRunHtmlMapper, createTextRunHtmlFn, TextRun } from "./ParagraphElements/TextRun";
+import { documentContentToHtmlMapper } from "./src";
+
+export class DocsToHtmlMapper{
+    static docToHtml(docsDocument:DocsDocumentModel, mapper:documentContentToHtmlMapper = HtmlMapper3.createHtml):Maybe<string>{
+        return DocsDocsDocumentAsMultipleParagraphs.getParagraphs(docsDocument)
+            .map(paragraphs => mapper(paragraphs, docsDocument?.inlineObjects))
+    }
+    
+    static concatDocs(...docs:DocsDocumentModel[]):string{
+        return docs.reduce((str:string, doc:DocsDocumentModel) => DocsToHtmlMapper.docToHtml(doc).map(html => str.concat(html)).orElse(str), " ").trim()
+    }
+}
+
+export interface IHtmlMapperConfigObj extends ImageHtmlMapper, TextRunHtmlMapper, ParagraphHtmlWrapper{}
+
+
+class HtmlMapper3{
+    static createHtml(documentContent:List<Paragraph>, imageProps?:DocsInlineObjectsModel, mapper:IHtmlMapperConfigObj = HtmlMapperConfigObj.initializeWithDefaults()):string{
+        return documentContent.map(paragraphToString(mapper, Maybe.of(imageProps)))
+            .map(mapper.wrapInParagraphTag)
+            .reduce(concatStrings)
+        
+        function paragraphToString(mapper: ParagraphElementHtmlMapper, imageProps:Maybe<DocsInlineObjectsModel>):(paragraph:Paragraph) => string{
+            return (paragraph:Paragraph) => paragraph.map(mapElementToHtml(mapper, imageProps)).reduce(concatStrings)
+        }
+
+        function mapElementToHtml(mapper: ParagraphElementHtmlMapper, imageProps:Maybe<DocsInlineObjectsModel>):(paragraphElement: DocsParagraphElementModel) => string {
+            return (paragraphElement:DocsParagraphElementModel) => createImageHtml(mapper, paragraphElement, imageProps)
+                .orElseGet(() => createTextRunHtml(mapper, paragraphElement).orElse("") );
+        }
+
+        function createTextRunHtml(mapper:TextRunHtmlMapper, paragraphElement:DocsParagraphElementModel):Maybe<string>{
+            return Maybe.of(paragraphElement?.textRun).map(mapper.createTextRunHtml)
+        }
+
+        function createImageHtml(mapper: ImageHtmlMapper, paragraphElement:DocsParagraphElementModel,imageProps:Maybe<DocsInlineObjectsModel>):Maybe<string>{
+            return curryLiftA2(mapper.createImageHtml, getInlineObjectElement(paragraphElement), imageProps)
+        }
+
+        function getInlineObjectElement(paragraphElement:DocsParagraphElementModel):Maybe<DocsInlineObjectElementModel>{
+            return Maybe.of(paragraphElement?.inlineObjectElement)
+        }
+    }
+}
+
+
+class HtmlMapperConfigObj implements IHtmlMapperConfigObj{
+    wrapInParagraphTag: wrapInParagraphTagFn;
+    createImageHtml: createImageHtmlFn;
+    createTextRunHtml: createTextRunHtmlFn;
+
+    constructor(wrapInParagraphTagFn: wrapInParagraphTagFn, createImageHtmlFn:createImageHtmlFn, createTextRunHtmlFn:createTextRunHtmlFn){
+        this.wrapInParagraphTag = wrapInParagraphTagFn
+        this.createImageHtml = createImageHtmlFn
+        this.createTextRunHtml = createTextRunHtmlFn
+    }
+
+    @bindClassMethodsToClassInstance
+    static of(wrapInParagraphTagFn: wrapInParagraphTagFn, createImageHtmlFn:createImageHtmlFn, createTextRunHtmlFn:createTextRunHtmlFn):HtmlMapperConfigObj{
+        return new HtmlMapperConfigObj(wrapInParagraphTagFn, createImageHtmlFn, createTextRunHtmlFn)
+    }
+    
+    static initializeWithDefaults():HtmlMapperConfigObj{
+        return HtmlMapperConfigObj.of(HtmlCreatorMapper.wrapInParagraphTag, Image.toHtml, TextRun.createHtml)
+    }
+}
---
+++ src/Main/HtmlCreator.ts
@@ -0,0 +1,20 @@
+import { DocsDocumentModel, DocsParagraphElementModel, DocsInlineObjectElementModel } from "../Models/DocsDocumentModel";
+import { DocsInlineObjectsModel } from "../Models/DocsInlineObjectModel";
+import { concatStrings } from "../Models/UtilityFunctions";
+
+import { ImageHtmlMapper, SizeInPixels } from "./ParagraphElements/Image";
+import { ParagraphHtmlWrapper } from "./ParagraphElements/ParagraphElements";
+import { TextRunHtmlMapper } from "./ParagraphElements/TextRun";
+
+
+export class HtmlCreatorMapper{
+    static wrapInImageTag(sourceUrl:string, sizeInPixels:SizeInPixels): string{
+        return `<img src="${sourceUrl}", height="${sizeInPixels.height}", width="${sizeInPixels.width}"></img>`
+    }
+    static wrapInLinkTag (link:string, text:string): string{
+        return `<a href="${link}" target="_blank">${text}</a>`
+    }
+    static wrapInParagraphTag(text:string):string{
+        return `<p>${text}</p>`
+    }
+}
