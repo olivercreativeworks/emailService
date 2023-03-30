5c2b51947473512109ef1dfc607a64284a33d7da
Oliver <olivercreativeworks@gmail.com>
2023-03-29 23:12:12 -0400

    Removed Outdated Files

--- src/Models/UtilityFunctions.ts
+++
@@ -1,6 +0,0 @@
-import { HtmlParagraph } from "../Main/Email";
-
-export function concatStrings(str1:HtmlParagraph, str2:HtmlParagraph):HtmlParagraph
-export function concatStrings(str1:string, str2:string):string{
-    return str1.concat(str2)
-}
