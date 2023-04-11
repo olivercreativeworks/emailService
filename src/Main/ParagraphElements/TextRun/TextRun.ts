cfe2c3c9d2f2d9fdd4a70ca3910d0c13d4c113b1
Oliver <olivercreativeworks@gmail.com>
2023-04-11 03:25:20 -0400

    Update text run to conform to paragraph element

--- src/Main/ParagraphElements/TextRun/TextRun.ts
+++ src/Main/ParagraphElements/TextRun/TextRun.ts
@@ -1,22 +1,32 @@
 import { DocsTextRunModel } from "../../../Models/DocsDocumentModel"
 import { Maybe } from "../../../Utility/Maybe"
+import { IElement } from "../ParagraphElements"
 
-export interface ITextRun{
-    text:Maybe<string>
+export interface ITextRun extends IElement<"textRun">{
+    text:string
     link:Maybe<string>
+    type: "textRun"
 }
-export class TextRun implements ITextRun{
-    text:Maybe<string>
-    link:Maybe<string>
 
-    constructor(text?:string, link?:string){
-        this.text = Maybe.of(text)
+
+export class TextRun implements ITextRun{
+    text: string
+    link: Maybe<string>
+    type: "textRun"
+
+    constructor(text:string, link?:string){
+        this.text = text
         this.link = Maybe.of(link)
+        this.type = "textRun"
     }
 
-    static of(docsTextRunModel: DocsTextRunModel):TextRun{
-        const text = docsTextRunModel?.content
-        const link = docsTextRunModel?.textStyle?.link?.url
+    static of(text:string, link?:string):TextRun{
         return new TextRun(text, link)
+    }
+
+    static from(docsTextRun: DocsTextRunModel):Maybe<TextRun>{
+        const text = docsTextRun?.content
+        const link = docsTextRun?.textStyle?.link?.url
+        return Maybe.of(text).map( (text: string) => TextRun.of(text, link) )
     }
 }
