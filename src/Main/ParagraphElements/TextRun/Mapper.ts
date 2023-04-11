237d7cfcabe2009235f64b2ff658fdbb9ef9b5f8
Oliver <olivercreativeworks@gmail.com>
2023-04-11 03:40:02 -0400

    Updated function signature. May revert.

--- src/Main/ParagraphElements/TextRun/Mapper.ts
+++ src/Main/ParagraphElements/TextRun/Mapper.ts
@@ -1,11 +1,8 @@
-import { curryLiftA2 } from "../../../Utility/Maybe";
 import { HtmlCreatorMapper } from "../../HtmlCreator";
-import { createLinkTagFn } from "../Image/HtmlMapper";
 import { ITextRun } from "./TextRun";
 
 export class TextRunMapper{
-    static createHtml(textRun:ITextRun, wrapInLinkTagFn: createLinkTagFn = HtmlCreatorMapper.wrapInLinkTag):string{
-        return curryLiftA2(wrapInLinkTagFn, textRun.link, textRun.text).orElseGet(() => textRun.text.orElse(""))
+    static createHtml(textRun:ITextRun):string{
+        return textRun.link.map(link => HtmlCreatorMapper.wrapInLinkTag(link, textRun.text)).orElse(textRun.text)
     }
 }
-
