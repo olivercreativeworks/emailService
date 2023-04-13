977f81835489f94bce568296a9ae8faea665261a
Oliver <olivercreativeworks@gmail.com>
2023-04-13 17:50:51 -0400

    Changed file location and renamed.

--- src/Main/ParagraphElements/TextRun/Mapper.ts
+++
@@ -1,8 +0,0 @@
-import { HtmlCreatorMapper } from "../../HtmlCreator";
-import { ITextRun } from "./TextRun";
-
-export class TextRunMapper{
-    static createHtml(textRun:ITextRun):string{
-        return textRun.link.map(link => HtmlCreatorMapper.wrapInLinkTag(link, textRun.text)).orElse(textRun.text)
-    }
-}
