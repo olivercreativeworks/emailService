b659338d29c80997f06593a63376cba8a88d130d
Oliver <olivercreativeworks@gmail.com>
2023-04-13 19:09:18 -0400

    Update method

--- src/Main/HtmlMapper/DocsToHtmlMapper.ts
+++ src/Main/HtmlMapper/DocsToHtmlMapper.ts
@@ -1,10 +1,12 @@
 import { List } from "../../Utility/List"
 import { HtmlCreatorMapper } from "./HtmlCreator"
 import { Document } from "../InterfaceLayer/Document"
-import { Html_Image } from "./Html_Image"
-import { ImageMapper } from "./ImageMapper"
+import { Html_Image } from "./ParagraphElements/Html_Image"
+import { Html_ImageMapper } from "./ParagraphElementMappers/Html_ImageMapper"
 import { ParagraphElement } from "../InterfaceLayer/ParagraphElements"
-import { TextRunMapper } from "./TextRunMapper"
+import { Html_TextRunMapper } from "./ParagraphElementMappers/Html_TextRunMapper"
+import { Html_TextRun } from "./ParagraphElements/Html_TextRun"
+import { LinkAttributes } from "./HtmlAttributes/LinkAttributes"
 
 function concatStrings(str1:string, str2:string):string{
     return str1.concat(str2)
@@ -18,7 +20,9 @@ export class HtmlMapper3{
             return htmlString.concat( HtmlCreatorMapper.createTag("p", list.reduce(concatStrings)) )
         } 
         function paragraphElementsToString(element: ParagraphElement): string{
-            return element.type == "image" ? ImageMapper.toHtml(Html_Image.from(element)) : TextRunMapper.createHtml(element)
+            return element.type == "image" ? 
+                Html_ImageMapper.toHtml(Html_Image.from(element), LinkAttributes.from) : 
+                Html_TextRunMapper.createHtml(Html_TextRun.from(element), LinkAttributes.from)
         }
     }
 }
