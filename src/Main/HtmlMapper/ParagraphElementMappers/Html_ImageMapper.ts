65c4956b90ded6fdd93b868d758f5ce2142d65e3
Oliver <olivercreativeworks@gmail.com>
2023-04-13 19:06:48 -0400

    Renamed file and updated method

---
+++ src/Main/HtmlMapper/ParagraphElementMappers/Html_ImageMapper.ts
@@ -0,0 +1,16 @@
+import { HtmlCreatorMapper } from "../HtmlCreator"
+import { ImageAttributes } from "../HtmlAttributes/ImageAttributes"
+import { IHtml_Image } from "../ParagraphElements/Html_Image"
+import { ILinkAttributes } from "../HtmlAttributes/LinkAttributes"
+
+type LinkAttributesFn = (link:string) => ILinkAttributes
+
+export class Html_ImageMapper{
+    static toHtml(image:IHtml_Image, createLinkAttributes:LinkAttributesFn):string{
+        const imgTag = HtmlCreatorMapper.createTag("img", null, ImageAttributes.from(image))
+        const linkAttributes = image.link.map(createLinkAttributes)
+        const htmlOutput = HtmlCreatorMapper.createLinkTagElseDefault(imgTag, linkAttributes)
+        
+        return htmlOutput
+    }
+}
