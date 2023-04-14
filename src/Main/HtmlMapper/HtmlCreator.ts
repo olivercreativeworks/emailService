f709ec3ab91e7317a5379b4c913d6ab996fbcbcd
Oliver <olivercreativeworks@gmail.com>
2023-04-14 01:06:05 -0400

    Update type name

--- src/Main/HtmlMapper/HtmlCreator.ts
+++ src/Main/HtmlMapper/HtmlCreator.ts
@@ -3,20 +3,20 @@ import { IImageAttributes } from "./HtmlAttributes/ImageAttributes"
 import { HtmlTags, IValidHtml } from "./IValidHtml"
 import { ILinkAttributes, LinkAttributes } from "./HtmlAttributes/LinkAttributes"
 
-type IAttributes = ILinkAttributes | IImageAttributes
+type IHtml_Attributes = ILinkAttributes | IImageAttributes
 
 export class HtmlCreatorMapper{
-    static createTag<A extends HtmlTags, B extends IAttributes>(tag:A, text?:string, attributes?:B):IValidHtml{
+    static createTag<A extends HtmlTags, B extends IHtml_Attributes>(tag:A, text?:string, attributes?:B):IValidHtml{
         return HtmlCreatorMapper.$createTag(tag, validateText(text), getAttributesString(attributes))
 
         function validateText(text?:string):string{
             return Maybe.of(text).orElse("")
         }
 
-        function getAttributesString(attributes:IAttributes):string{
+        function getAttributesString(attributes:IHtml_Attributes):string{
             return Maybe.of(attributes).map(toString).orElse("")
 
-            function toString(attributes:IAttributes):string{
+            function toString(attributes:IHtml_Attributes):string{
                 return Object.entries(attributes).map(joinAttributeNameAndValue).join(", ")
             }
             function joinAttributeNameAndValue([attributeName, attributeValue]:[string, string]):string{
