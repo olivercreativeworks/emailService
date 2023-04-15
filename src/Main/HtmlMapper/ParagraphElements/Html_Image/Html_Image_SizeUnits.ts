b97561f3b4c82859c83f64b294dcdf74cfd167f0
Oliver <olivercreativeworks@gmail.com>
2023-04-15 18:20:37 -0400

    Move Size class to own file in interface layer

--- src/Main/HtmlMapper/ParagraphElements/Html_Image/Html_Image_SizeUnits.ts
+++ src/Main/HtmlMapper/ParagraphElements/Html_Image/Html_Image_SizeUnits.ts
@@ -1,31 +1,8 @@
-import { DocsInlineObjectSizeModel } from "../../../../Models/DocsDocumentModel"
-
 type sizeUnits = "pixel"
+
 interface ISize<unitOfMeasure extends sizeUnits>{
     height:number
     width:number
     unit:unitOfMeasure
 }
 export type ISizeInPixels = ISize<"pixel">
-
-export class SizeInPixels implements ISizeInPixels{
-    height:number
-    width:number
-    unit:"pixel" = "pixel"
-
-    static of(width:number, height:number):SizeInPixels{
-        return new SizeInPixels(width, height)
-    }
-    static convertDocsSizeToPixelSize(size:DocsInlineObjectSizeModel):SizeInPixels{
-        const widthInPoints = size?.width?.magnitude
-        const heightInPoints = size?.height?.magnitude
-        return SizeInPixels.of( SizeInPixels.convertPointSizetoPixelSize(widthInPoints), SizeInPixels.convertPointSizetoPixelSize(heightInPoints) )
-    }
-    private static convertPointSizetoPixelSize(pointSize:number):number{
-        return pointSize * (4/3)
-    }
-    constructor(width:number, height:number){
-        this.height = height
-        this.width = width
-    }
-}
