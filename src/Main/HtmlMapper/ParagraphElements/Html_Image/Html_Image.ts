f18e08f6e9d6d684eb396e8c04bd6d5e6afc51c0
Oliver <olivercreativeworks@gmail.com>
2023-04-15 18:21:11 -0400

    Update interface name and method signatures.

--- src/Main/HtmlMapper/ParagraphElements/Html_Image/Html_Image.ts
+++ src/Main/HtmlMapper/ParagraphElements/Html_Image/Html_Image.ts
@@ -1,32 +1,27 @@
 import { Maybe } from "../../../../Utility/Maybe"
-import { IImage } from "../../../InterfaceLayer/ImageObj"
 import { IHtml_ParagraphElementType } from "../Html_ParagraphElements"
-import { SizeInPixels } from "./Html_Image_SizeUnits"
+import { ISizeInPixels } from "./Html_Image_SizeUnits"
 
 export interface IHtml_Image{
     sourceUrl:string
-    size:SizeInPixels
+    size:ISizeInPixels
     link:Maybe<string>
 }
 
 export class Html_Image implements IHtml_Image, IHtml_ParagraphElementType<"html_image">{
     sourceUrl: string
-    size: SizeInPixels
+    size: ISizeInPixels
     link: Maybe<string>
     type: "html_image"
 
-    constructor(sourceUrl:string, size:SizeInPixels, link?:string){
+    constructor(sourceUrl:string, size:ISizeInPixels, link:Maybe<string>){
         this.sourceUrl = sourceUrl
         this.size = size
-        this.link = Maybe.of(link)
+        this.link = link
         this.type = "html_image"
     }
 
-    static of(sourceUrl:string, size:SizeInPixels, link?:string):Html_Image{
+    static of(sourceUrl:string, size:ISizeInPixels, link:Maybe<string>):Html_Image{
         return new Html_Image(sourceUrl, size, link)
-    }
-
-    static from(image:IImage):Html_Image{
-        return Html_Image.of(image.sourceUrl, SizeInPixels.convertDocsSizeToPixelSize(image.size), image.link.unsafeUnwrap())
     }
 }
