87ce6dad17e9442da26327ba8a41dbbdea1cabd0
Oliver <olivercreativeworks@gmail.com>
2023-04-16 06:20:38 -0400

    Update main

--- src/Code.ts
+++ src/Code.ts
@@ -1,24 +1,18 @@
-import { Email, sendEmail } from "./Main/Email"
-import { DocsDocumentToHtmlMapper } from "./Main/DocsToHtml/DocsMapper/DocsDocumentToHtmlMapper"
-import { DocsDocument } from "./Main/InterfaceLayer/DocsElements/DocsDocument"
-import { DocsDocumentModel } from "./Main/Models/DocsDocumentModel"
-import { List } from "./Utility/List"
-import { Utility } from "./Utility/Utility"
-import { DocsToHtmlDocumentMapper } from "./Main/InterfaceLayer/MappersToDomainElements/HtmlDocumentMapper"
+import { DocsDocumentModel } from "./_Simple/Models/DocsDocumentModel"
+import { HtmlConverter } from "./_Simple/OneFile"
+import { Funcs } from "./Utility/Utility"
 
 function main(){
     const doc1 = Docs.Documents.get('1Y29ar26MwC5tYw1-fLoh2Ndget5jMLUcGOdwmMd3vwo') as DocsDocumentModel
     const doc2 = Docs.Documents.get('1-ngp__00XaqhpnNaSJ4xXDBn7UKkAxM-kdab1a5avWI') as DocsDocumentModel
-
-    const myDocument = DocsToHtmlDocumentMapper.from(doc1).map(DocsDocumentToHtmlMapper.createHtml)
-    const myDocument2 = DocsToHtmlDocumentMapper.from(doc2).map(DocsDocumentToHtmlMapper.createHtml)
-    // const myDocument = DocsDocument.from(doc1).map(DocsDocumentToHtmlMapper.createHtml)
-    // const myDocument2 = DocsDocument.from(doc2).map(DocsDocumentToHtmlMapper.createHtml)
-    Logger.log(myDocument)
-    Logger.log(myDocument2 )
-
-    const docsAsHtml = Utility.liftA2(doc1 => doc2 => doc1.concat(doc2), myDocument, myDocument2)
-    docsAsHtml.map(Logger.log)
+    const convertedDoc1 = HtmlConverter.convertDocToHtml(doc1)
+    const convertedDoc2 = HtmlConverter.convertDocToHtml(doc2)
+    const concatDocs = Funcs.curryLiftA2(
+        Funcs.concatStrings, 
+        convertedDoc1, 
+        convertedDoc2
+    )
+    concatDocs.map(Logger.log)
 
     // // Utility.liftA2(x=>y=> x.concat(y), myDocument, myDocument2).map(Logger.log)
     // // const docsHtml = concatDocs(doc1, doc2)
@@ -28,6 +22,9 @@ function main(){
     
     // docsAsHtml.map(html => sendEmail(recipients, subjectLine, html))
 }
+
+
+
 
 function doGet(){
     return HtmlService.createHtmlOutputFromFile("src/index")
