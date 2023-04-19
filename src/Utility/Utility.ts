8b32353675bcab8eff2b2d77dbe2067517f28044
Oliver <olivercreativeworks@gmail.com>
2023-04-18 22:56:43 -0400

    Add generic map function

--- src/Utility/Utility.ts
+++ src/Utility/Utility.ts
@@ -1,4 +1,4 @@
-import { Applicative } from "./Interfaces"
+import { Applicative, Monad } from "./Interfaces"
 import { List } from "./List"
 import { Maybe } from "./Maybe"
 
@@ -41,5 +41,60 @@ export namespace Funcs{
     export function curryLiftA3<A,B,C,D>(fn:(arg1:A, arg2:B, arg3:C) => D, applicative1:Maybe<A>, applicative2:Maybe<B>, applicative3:Maybe<C>):Maybe<D>{
         return liftA3(curryA3(fn), applicative1, applicative2, applicative3)
     }
+
+    type MonadMethodReturnType<A extends Monad<unknown>, methodName extends keyof Monad<unknown>> = ReturnType< A[methodName]>
+    type MonadType<ContainerType extends Monad<unknown>, ValueType> = MonadMethodReturnType<ContainerType, "map"> & Monad<ValueType>
+
+    export function map<A,B,C extends Monad<B>>(fn:(arg:A) => B, returnType:(arg:B) => C): (monad:MonadType<C, A>) => C  {
+        return (monad:MonadType<C,A>) => monad.map(fn) as C
+    }
 }
+
+
+
+// function testingMap(){
+//     const cat = Maybe.of(1)
+//     const kitten = List.of(2)
+//     const add = (x:number) => x + 1
+//     const dog = cat.map(add)
+//     const dog2 = map(add, cat)
+//     const dog3 = map(add, kitten)
+//     const dog4 = Funcs.curryA2(map)(add)(cat)
+//     const dog5 = Funcs.curryA2(map)()
+//     const elephant = greet("j", ["l"])
+//     const elephant1 = Funcs.curryA2(greet)(3)("j")
+//     const dog6 = map(add, Maybe.of) (Maybe.of(4))
+//     const dog7 = Funcs.curryA3(map)(add)(Maybe.of)()
+// }
+
+// function map2(fn, moand){
+//     return moand.map(fn)
+// }
+// // type MakeReturnTypeAny = ReturnType<() => any>
+// // type MakeGeneric<A extends Monad<string>, B extends (arg:any) => Monad<any>> = 
+
+// // function map<A, B>(fn:(arg:A) => B, maybe:Maybe<A>): Maybe<B>
+// // function map<A, B>(fn:(arg:A) => B, list:List<A>): List<B>
+// // function map(fn:(arg:unknown) => unknown, monad:Monad<unknown>): unknown{
+// //     return monad.map(fn)
+// // }
+
+
+// // function map<A,B,C extends Monad<B>, D extends Monad<A> >(fn:(arg:A) => B, returnType:(arg:B) => C ): (monad:D & MapReturnType<C>) => C  {
+// //     return (monad:D & MapReturnType<C>) => monad.map(fn) as C
+// // }
+
+// function greet(x:number, person: string): string;
+// function greet(x:string, persons: string[]): string[];
+ 
+// // Implementation signature
+// function greet(x:unknown, person: unknown): unknown {
+//   if (typeof person === 'string' && typeof x =="string") {
+//     console.log(x)
+//     return `Hello, ${person}!`;
+//   } else if (Array.isArray(person)) {
+//     return person.map(name => `Hello, ${name}!`);
+//   }
+//   throw new Error('Unable to greet');
+// }
 
