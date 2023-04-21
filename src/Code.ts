73d831c4b469fc6c3d4d2366e77ba963a4a5a2a8
Oliver <olivercreativeworks@gmail.com>
2023-04-21 09:18:02 -0400

    Removed outdated comments

--- src/Code.ts
+++ src/Code.ts
@@ -2,8 +2,6 @@ import { DocsDocumentModel } from "./_Simple/DocsDocumentModel"
 import { Email } from "./_Simple/Email"
 import { HtmlConverter } from "./_Simple/OneFile"
 import { List } from "./Utility/List"
-import { Funcs } from "./Utility/Utility"
-
 
 function main(){
     const docs = getIds().map(getDoc)
@@ -12,29 +10,10 @@ function main(){
         List.of("oliver@urbanupbound.org"),
         "Testing this new email",
         htmlBody
-    )).map(Email.sendEmail)
-
-    // sendEmail()
-    // const doc1 = Docs.Documents.get('1Y29ar26MwC5tYw1-fLoh2Ndget5jMLUcGOdwmMd3vwo') as DocsDocumentModel
-    // const doc2 = Docs.Documents.get('1-ngp__00XaqhpnNaSJ4xXDBn7UKkAxM-kdab1a5avWI') as DocsDocumentModel
-    // const convertedDoc1 = HtmlConverter.convertDocToHtml(doc1)
-    // const convertedDoc2 = HtmlConverter.convertDocToHtml(doc2)
-    // const concatDocs = Funcs.curryLiftA2(
-    //     Funcs.concatStrings, 
-    //     convertedDoc1, 
-    //     convertedDoc2
-    // )
-    // concatDocs.map(Logger.log)
-    
-    // const email = Email.of(emailAttributes).send()
-    // // Utility.liftA2(x=>y=> x.concat(y), myDocument, myDocument2).map(Logger.log)
-    // // const docsHtml = concatDocs(doc1, doc2)
-    // // // const docsHtml = DocsToHtmlMapper.concatDocs(doc1, doc2)
-    // const subjectLine =  "Testing this new email"
-    // const recipients:List<Email> = List.of("oliver@urbanupbound.org")
-    
-    // docsAsHtml.map(html => sendEmail(recipients, subjectLine, html))
+    ))
+    email.map(Email.sendEmail)
 }
+
 function getDoc(docId:string):DocsDocumentModel{
     return Docs.Documents.get(docId) as DocsDocumentModel
 }
