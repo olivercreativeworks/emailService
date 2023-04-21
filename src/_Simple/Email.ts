224de78f6345004af3ddad78d28d62e47d22e9b2
Oliver <olivercreativeworks@gmail.com>
2023-04-21 10:18:03 -0400

    Simplified functions

--- src/_Simple/Email.ts
+++ src/_Simple/Email.ts
@@ -1,6 +1,5 @@
 import { List } from "../Utility/List"
 
-
 export type EmailAddress = `${string}@${string}`
 
 interface IEmail{
@@ -9,30 +8,18 @@ interface IEmail{
     htmlBody: string
 }
 
-class EmailSubjectLengthValidator{
-    static getMaxSubjectLength(){
-        return 250
-    }
-    static validate(subject:string, throwErrorFn: (maxLength:number) => never):string | never{
-        const maxLength = EmailSubjectLengthValidator.getMaxSubjectLength()
-        return subject.length < maxLength ? subject : throwErrorFn(maxLength)
-    }
-}
-
-class EmailSubjectValidatonError{
-    static subjectIsTooLongError(maxLength:number):never{
-        throw new Error(`The subject must be ${maxLength} characters or less.`)
-    }
-}
-
 export namespace Email{
+    
     export function createEmail(recipients:List<EmailAddress>, subject:string, htmlBody:string):IEmail{
-        const validSubject = EmailSubjectLengthValidator.validate(subject, EmailSubjectValidatonError.subjectIsTooLongError)
-        return {recipients, subject:validSubject, htmlBody}
+        return {recipients, subject, htmlBody}
     }
     
-    export function sendEmail(email:IEmail):void{
-        const recipientsAsString = email.recipients.asArray().join(",")
-        GmailApp.sendEmail(recipientsAsString, email.subject, null, {"htmlBody":email.htmlBody,"name":"Oliver Allen-Cummings"})
+    export function sendEmail(email:IEmail, name:string):void{
+        const recipients = email.recipients.toString(",")
+        const subject = email.subject
+        const body = null
+        const htmlBody = email.htmlBody
+        
+        GmailApp.sendEmail(recipients, subject, body, {htmlBody, name})
     }
 }
