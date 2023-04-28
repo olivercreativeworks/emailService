8b8e28ddc0640ab02d4ef3364ffc1997b3ad6047
Oliver <olivercreativeworks@gmail.com>
2023-04-28 13:13:42 -0400

    Updated imports

--- src/Code.ts
+++ src/Code.ts
@@ -1,21 +1,24 @@
-import { DocsDocumentModel } from "./_Simple/DocsDocumentModel"
-import { Email } from "./_Simple/Email"
-import { HtmlConverter } from "./_Simple/OneFile"
-import { List } from "./Utility/List"
+import { GoogleDocs } from "./_Simple/GoogleDocsDocumentModel"
+import { HtmlMapper } from "./_Simple/HtmlConverter"
+
 
 function main(){
     const docs = getIds().map(getDoc)
-    const htmlBody = HtmlConverter.docsToHtml(...docs)
-    const email = htmlBody.map(htmlBody => Email.createEmail(
-        List.of("oliver@urbanupbound.org"),
-        "Testing this new email",
-        htmlBody
-    ))
-    email.map(Email.sendEmail)
+    const htmlBody = HtmlMapper.docsToHtml(...docs)
+    Logger.log(htmlBody)
+    if (htmlBody.length == 0){
+        
+    }
+    // const email = Email.createEmail(
+    //     List.of("oliver@urbanupbound.org"),
+    //     "Testing this new email",
+    //     htmlBody
+    // )
+    // Email.sendEmail(email, "Oliver Allen-Cummings")
 }
 
-function getDoc(docId:string):DocsDocumentModel{
-    return Docs.Documents.get(docId) as DocsDocumentModel
+function getDoc(docId:string):GoogleDocs.DocumentModel{
+    return Docs.Documents.get(docId) as GoogleDocs.DocumentModel
 }
 function getIds(){
     return ['1Y29ar26MwC5tYw1-fLoh2Ndget5jMLUcGOdwmMd3vwo', '1-ngp__00XaqhpnNaSJ4xXDBn7UKkAxM-kdab1a5avWI']
